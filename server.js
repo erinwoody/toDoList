57dcebdeaacc96c0af441c8c4304ccd2fb013a7f
Erin Woody <erinelizabethwoody@gmail.com>
2017-08-21 22:39:38 -0500

    added css

--- server.js
+++ server.js
@@ -14,24 +14,37 @@ app.set("views", "./views");
 app.set("view engine", "mustache");
 
 app.use(express.static(path.join(__dirname, "./public")));
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({
+    extended: true
+
+}));
 
 let todos = [];
 
-app.get("/", (req, res) => {
-    res.render('index', { todos: todos });
+app.get("/", function (req, res) {
+    res.render('index', {
+        todos: todos
+    });
 });
 
 app.post("/newtodo", (req, res) => {
     let newTodo = req.body;
     newTodo.complete = false;
     todos.push(newTodo);
+    // console.log('todos: ', todos);
     res.redirect("/");
 });
 
-
+app.post("/complete/:todo", (req, res) => {
+    let task = req.params.todo;
+    let foundTodo = todos.find(function (todo) {
+        return todo.task === task;
+    });
+    foundTodo.complete = !foundTodo.complete;
+    res.redirect("/")
+})
 
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
 });
-
+  
